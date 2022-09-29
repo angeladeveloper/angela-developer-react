df36d5aa9c08115b672363c5c483b2062456e75b
Angela W <squittenplease@gmail.com>
2022-09-28 18:55:10 -0700

    Functional portfolio, needs MAJOR styling changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,10 @@ function NavigationBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="https://github.com/angeladeveloper">GitHub🚀</Nav.Link>
-            <Nav.Link href="https://www.linkedin.com/in/angelag6617/">Linkdin🚀</Nav.Link>
             <Nav.Link href="/aboutme">About Me</Nav.Link>
             <Nav.Link href="/projects">Projects</Nav.Link>
+            <Nav.Link href="https://github.com/angeladeveloper">GitHub🚀</Nav.Link>
+            <Nav.Link href="https://www.linkedin.com/in/angelag6617/">Linkdin🚀</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
